f9f298bd0f5d618eab0524dc22ffbcfe212a4529
metlandus <metlandus@hotmail.com.tr>
2024-08-18 15:09:41 +0300

    Task 4 and 5 are done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
+const mail = document.querySelector('input[type="email"]');
+const password = document.querySelector('input[type="password"]');
+
+form.addEventListener('submit', () => {
+    event.preventDefault();
+    if (mail.value === "" || password.value === "") {
+        alert('All form fields must be filled in');
+    }
+    else {
+        let logInfo = {}
+        logInfo.email = mail.value;
+        logInfo.password = password.value;
+        console.log(logInfo);
+    }
+    form.reset();
+});
 
