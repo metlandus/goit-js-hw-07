085a4d6ce2483ebac44ffe7b22940191c0badb96
metlandus <metlandus@hotmail.com.tr>
2024-08-20 23:52:32 +0300

    Task 6 is done

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
 		.toString(16)
 		.padStart(6, 0)}`;
 }
+let input = document.querySelector('#controls input');
+const createBtn = document.querySelector('#controls button[data-create]');
+const buttonDestroy = document.querySelector('#controls button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', () => {
+	const amount = parseInt(input.value, 10);
+	createBoxes(amount);
+});
+
+
+function createBoxes(amount) {
+	// check if amount is in the correct range
+	if (amount >= 1 && amount <= 100) {
+		console.log(amount);
+		for (let i = 0; i < amount; i++) {
+			// create a box for each amount
+			const box = document.createElement('div');
+			box.style.width = `${30 + i * 10}px`;
+			box.style.height = `${30 + i * 10}px`;
+			box.style.backgroundColor = getRandomHexColor();
+			// place the boxes in the boxes div
+			boxes.appendChild(box);
+		}
+	}
+}
+
+const destroyBoxes = () => {
+	// remove all boxes from the boxes div
+	boxes.innerHTML = ''; 	
+}
+createBtn.addEventListener('click', createBoxes(input));
+buttonDestroy.addEventListener('click', destroyBoxes);
